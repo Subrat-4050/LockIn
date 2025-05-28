3260b0371a19facb43256c4679028764114afabf
Subrat-4050 <sahusubrat83184@gmail.com>
2025-05-28 08:30:48 +0530

    add background and implement login logic

---
+++ app/src/main/java/com/example/lockin/HomeScreen.kt
@@ -0,0 +1,25 @@
+package com.example.lockin
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.rememberNavController
+import com.example.lockin.ui.theme.GreenJc
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+
+}
+
--- app/src/main/java/com/example/lockin/MainActivity.kt
+++ app/src/main/java/com/example/lockin/MainActivity.kt
@@ -1,16 +1,49 @@
 package com.example.lockin
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavGraph
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.lockin.ui.theme.GreenJc
 import com.example.lockin.ui.theme.LockInTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,8 +52,107 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LockInTheme {
-
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .paint(
+                            painter = painterResource(id = R.drawable.loginbg),
+                            contentScale = ContentScale.FillBounds
+                        )
+                ) {
+                    val navController = rememberNavController()
+                    NavGraph(navController = navController)
+                }
             }
         }
     }
 }
+
+//@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun LoginScreen(onLoginSuccess: () -> Unit = {}) {
+    var userName by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val context = LocalContext.current.applicationContext
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 26.dp, vertical = 140.dp),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        OutlinedTextField(value = userName , onValueChange = {userName = it},
+            label = { Text(text = "UserName") },
+            shape = RoundedCornerShape(20.dp),
+            colors = TextFieldDefaults.colors(
+                focusedLeadingIconColor = GreenJc,
+                unfocusedLeadingIconColor = GreenJc,
+                focusedLabelColor = GreenJc,
+                unfocusedLabelColor = GreenJc,
+                
+            ),
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Person, contentDescription = "Person")
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp)
+        )
+
+        OutlinedTextField(value = password , onValueChange = {password = it},
+            label = { Text(text = "Password") },
+            shape = RoundedCornerShape(20.dp),
+            colors = TextFieldDefaults.colors(
+                focusedLeadingIconColor = GreenJc,
+                unfocusedLeadingIconColor = GreenJc,
+                focusedLabelColor = GreenJc,
+                unfocusedLabelColor = GreenJc,
+            ),
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Lock, contentDescription = "Lock")
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp),
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Button(onClick = {
+            if(authentication(userName,password)) {
+                onLoginSuccess()
+                Toast.makeText(context, "Login Successful", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(context, "Login Failed", Toast.LENGTH_SHORT).show()
+            }
+        },
+            colors = ButtonDefaults.buttonColors(GreenJc),
+            contentPadding = PaddingValues(horizontal = 60.dp, vertical = 8.dp),
+            modifier = Modifier.padding(top = 18.dp)
+        ) {
+            Text(text = "Login", fontSize = 22.sp)
+        }
+    }
+}
+
+private fun authentication(userName: String, password: String) : Boolean {
+    return userName == "admin" && password == "admin@1234"
+}
+
+@Composable
+fun NavGraph(navController : NavHostController) {
+    NavHost(navController = navController, startDestination = "login") {
+        composable("login") {
+            LoginScreen(onLoginSuccess = {
+                navController.navigate("home") {
+                    popUpTo(0)
+                }
+            })
+        }
+        composable("home") {
+            HomeScreen()
+        }
+    }
+}
+
+
